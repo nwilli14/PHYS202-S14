79b3025c153adf583d600ef55cdb0269fd82abfa
Nick Williams <nwilli14@calpoly.edu>
2014-05-13 13:59:18 -0700

    Integration exercises, incomplete

---
+++ SciPy/integrators.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def traps(func, a, b, N):
+    """docstring"""
+    h = (b-a)/N
+    k = np.arange(1,N)
+    I = h*(0.5*func(a) + 0.5*func(b) + func(a+k*h).sum())
+    #print "Trap rule integral ="
+    return I
+    
+    
+def simps(func, a, b, N):
+    """docstring"""
+    h = (b-a)/N
+    
+    k1 = np.arange(1,N/2+1)
+    K2 = np.arange(1,N/2)
+    I = (1./3.)*h*(func(a) +func(b) + 4.*func(a+(2*k1-1)*h).sum() + 2.*func(a+2*K2*h).sum())
+    #print "simps rule integral ="
+    return I
