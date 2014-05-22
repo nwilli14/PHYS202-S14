3fcd7d47cd5e1c904b9ef05dc876264e0163f7ea
nwilli14 <nwilli14@calpoly.edu>
2014-05-21 23:26:51 -0700

    exercises need completing

--- SciPy/integrators.py
+++ SciPy/integrators.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def traps(func, a, b, N):
-    """docstring"""
+    """This takes a function and approximates the definite integral
+    from a to b by using N trapizodes
+    h is the width of each trapizode"""
     h = (b-a)/N
     k = np.arange(1,N)
     I = h*(0.5*func(a) + 0.5*func(b) + func(a+k*h).sum())
@@ -10,7 +12,8 @@ def traps(func, a, b, N):
     
     
 def simps(func, a, b, N):
-    """docstring"""
+    """This takes a function and approximates the definite integral
+    form a to b by using N quadratics"""
     h = (b-a)/N
     
     k1 = np.arange(1,N/2+1)
